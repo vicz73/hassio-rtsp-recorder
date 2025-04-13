a46ee2d22ca89f4898ad38058bb1351f79ff13f4
vicz73 <154098516+vicz73@users.noreply.github.com>
2025-04-13 22:40:01 +0200

    aggiustamenti

--- rtsp-recorder/run.sh
+++ rtsp-recorder/run.sh
@@ -5,15 +5,14 @@ OUTPUT_PATH=$(jq -r '.output_path' /data/options.json)
 CLIP_DURATION=$(jq -r '.clip_duration' /data/options.json)
 RETENTION_DAYS=$(jq -r '.retention_days' /data/options.json)
 
+echo "Using RTSP_URL=$RTSP_URL and OUTPUT_PATH=$OUTPUT_PATH"
+
 mkdir -p "$OUTPUT_PATH"
 
 while true; do
   TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
-  ffmpeg -rtsp_transport tcp -i "$RTSP_URL" \
-    -t "$CLIP_DURATION" \
-    -vcodec copy -acodec copy \
-    "$OUTPUT_PATH/$TIMESTAMP.mp4"
+  ffmpeg -rtsp_transport tcp -i "$RTSP_URL" -t "$CLIP_DURATION" -vcodec copy -acodec copy "$OUTPUT_PATH/$TIMESTAMP.mp4"
 
   # Rimuove file più vecchi di X giorni
   find "$OUTPUT_PATH" -type f -mtime +"$RETENTION_DAYS" -delete
